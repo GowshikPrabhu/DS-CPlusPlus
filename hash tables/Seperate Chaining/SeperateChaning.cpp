4a09305ef200fc339bf8d16a1844df60c24e6c01
Gowshik Prabhu <gowshikprabhu@gmail.com>
2020-09-11 23:50:33 +0530

    Create file for seperate chaining

---
+++ hash tables/Seperate Chaining/SeperateChaning.cpp
@@ -0,0 +1,6 @@
+/*
+* Create a hash function, such that our hash table has ‘N’ number of buckets.
+* To insert a node into the hash table, we need to find the hash index for the given key. And it
+* could be calculated using the hash function. Use seperate chaining for collision.
+* Example: hashIndex = key % noOfBuckets
+*/
